37fcc5a5ecb855e317f996b1e3975bffab08b03b
Victor Ferreira <69025753+Polymatheia-BR@users.noreply.github.com>
2023-09-29 22:46:10 -0300

    Update exercicio16.py

--- exercicio16.py
+++ exercicio16.py
@@ -1,8 +1,9 @@
 hora_inicio = int(input('Digite a hora de inicio da partida de xadrez: '))
 hora_fim = int(input('Digite a hora do fim da partida de xadrez: '))
 
-if hora_inicio > hora_fim:
-    hora_inicio -= 24
-    print(hora_fim - hora_inicio)
+if hora_inicio >= hora_fim:
+    resultado = hora_fim - (hora_inicio - 24)
 else:
-    print(hora_fim - hora_inicio)
+    resultado = hora_fim - hora_inicio
+
+print(f'A duração do jogo foi de {resultado}')
